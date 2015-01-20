90c5fab35b5d7e296ab92b84283818854e7719d3
uncle <434967007@qq.com>
2015-01-20 08:54:50 +0800

    方法整理

--- src/InstallApk.java
+++ src/InstallApk.java
@@ -31,20 +31,16 @@ public class InstallApk extends JFrame {
 						@SuppressWarnings("rawtypes")
 						Iterator itor = lt.iterator();
 						while (itor.hasNext()) {
-							
+
 							File f = (File) itor.next();
 							String filePath = f.getAbsolutePath();
 							if (!filePath.endsWith(".apk")) {
-								System.out.println("拖拽的不是apk文件,手不要抖哦...");
 								alertError("拖拽的不是apk文件,手不要抖哦...");
-								initJta();
 								continue;
 							} else {
 								List<String> devices = ADBUtil.getDevices();
 								if (devices.size() != 1) {
-									System.out.println("未发现设备或发现了不只一个设备！");
 									alertError("未发现设备或发现了不只一个设备！");
-									initJta();
 									continue;
 								} else {
 									jta.setText("正在安装... ");
@@ -54,32 +50,20 @@ public class InstallApk extends JFrame {
 									result = result.toLowerCase();
 									if (result.contains("success")) {
 										alertInfo("安装成功！");
-										System.out.println("安装成功！");
-										initJta();
 										continue;
 									} else {
 										if (result
 												.contains("install_failed_already_exists")) {
-											System.out
-													.println("该apk已经存在，请先卸载！");
 											alertError("该应用已经存在，请先卸载！");
-											initJta();
 											continue;
 										} else if (result.contains("")) {
-											System.out.println("签名冲突,安装失败！");
 											alertError("签名冲突,安装失败！");
-											initJta();
 											continue;
 										} else if (result.contains("'adb'")) {
-											System.out
-													.println("抱歉，您的电脑未安装 Android SDK，暂时无法使用。");
 											alertError("抱歉，您的电脑未安装 Android SDK，暂时无法使用。");
-											initJta();
 											continue;
 										} else {
 											alertError("安装失败！请重试...");
-											System.out.println("安装失败！请重试...");
-											initJta();
 											continue;
 										}
 									}
@@ -113,6 +97,8 @@ public class InstallApk extends JFrame {
 	public void alertError(String message) {
 		JOptionPane.showMessageDialog(null, message, "错误",
 				JOptionPane.ERROR_MESSAGE);
+		System.out.println("message");
+		initJta();
 
 	}
 
@@ -122,14 +108,17 @@ public class InstallApk extends JFrame {
 	 * @param message
 	 */
 	public void alertInfo(String message) {
-		JOptionPane.showMessageDialog(null, message, "提示",JOptionPane.INFORMATION_MESSAGE);
+		JOptionPane.showMessageDialog(null, message, "提示",
+				JOptionPane.INFORMATION_MESSAGE);
+		System.out.println("message");
+		initJta();
 	}
 
+	/**
+	 * 重置文本域
+	 */
 	public void initJta() {
 		jta.setText(INFO);
 	}
 
-	public void aa() {
-
-	}
 }
